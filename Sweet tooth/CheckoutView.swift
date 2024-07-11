f0071af9ee50b93c4abefb66cb070449be3ac6be
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-11 12:21:53 -0400

    Send and receive order over internet

--- Sweet tooth/CheckoutView.swift
+++ Sweet tooth/CheckoutView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct CheckoutView: View {
     
     var order: Order
-    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     var body: some View {
         ScrollView {
             VStack {
@@ -25,7 +26,11 @@ struct CheckoutView: View {
                 
                 Text("your total cost is \(order.cost, format: .currency(code: "USD"))")
                 
-                Button("Place Order", action: {})
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
                     .padding()
             }
             
@@ -33,6 +38,36 @@ struct CheckoutView: View {
         .navigationTitle("Check Out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank You!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+        
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            if let response = try? JSONDecoder().decode(Order.self, from: data) {
+                confirmationMessage = "your order for \(response.quatity)x \(Order.types[response.type].lowercased()) cupcakes is on it's way!"
+                showingConfirmation = true
+            } else {
+                print("Failed to decode response")
+            }
+        } catch {
+            print("Check out failed")
+        }
     }
 }
 
