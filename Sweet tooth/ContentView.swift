0057d2132298c71cf72a0cde733d5a0244db5472
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-11 11:21:55 -0400

    CoreHaptics

--- Sweet tooth/ContentView.swift
+++ Sweet tooth/ContentView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
-
+import CoreHaptics
 
 struct ContentView: View {
-    
+    @State private var count = 0
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
         AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
@@ -26,6 +27,58 @@ struct ContentView: View {
         }
         .frame(width: 200)
         
+        Button("Taps: \(count)") {
+            count += 1
+        }
+        .sensoryFeedback(.increase, trigger: count)
+        
+        Button("Tap Me", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creatng the engine")
+        }
+        
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern")
+        }
     }
 }
 #Preview {
