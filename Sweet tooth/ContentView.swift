40f30d937575bc83ad15338c38c1db59810850d1
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-11 10:17:15 -0400

    Initial Commit

---
+++ Sweet tooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sweet tooth
+//
+//  Created by Parth Antala on 2024-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sweet tooth/Sweet_toothApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_toothApp.swift
+//  Sweet tooth
+//
+//  Created by Parth Antala on 2024-07-11.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_toothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
