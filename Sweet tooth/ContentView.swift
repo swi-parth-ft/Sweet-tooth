a11b9fe3003feefc928c0a15bba27a3d78695058
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-11 10:32:44 -0400

    Try fetching data from itunes api

--- Sweet tooth/ContentView.swift
+++ Sweet tooth/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    
+    @State private var results = [Result]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
+            .task {
+                await loadData()
+            }
         }
         .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=queen&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedData.results
+            }
+            
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
 
