12a6e717a1bae8db186123417d726270eb8da65b
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-11 10:41:50 -0400

    Load Image from remote server

--- Sweet tooth/ContentView.swift
+++ Sweet tooth/ContentView.swift
@@ -7,55 +7,27 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
     
-    @State private var results = [Result]()
-    var body: some View {
-        VStack {
-            List(results, id: \.trackId) { item in
-                VStack(alignment: .leading) {
-                    Text(item.trackName)
-                        .font(.headline)
-                    Text(item.collectionName)
-                }
-            }
-            .task {
-                await loadData()
-            }
-        }
-        .padding()
-    }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=queen&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedData.results
+    var body: some View {
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("Error loading the picture")
+            } else {
+                ProgressView()
             }
             
-        } catch {
-            print("Invalid data")
         }
+        .frame(width: 200)
         
     }
 }
-
 #Preview {
     ContentView()
 }
